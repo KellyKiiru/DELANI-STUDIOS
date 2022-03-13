b31f3173dad9cad15d45838813349f3b6f442c8a
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-03-13 11:21:21 +0300

    delete commented jquery

--- javascript/main.js
+++ javascript/main.js
@@ -47,22 +47,6 @@ $(document).ready(function () {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function () {
   $('#uno').mouseover(function () {
     $('#dark-screen1').show(1200);
@@ -73,261 +57,76 @@ $(document).ready(function () {
 });
 
 
-// $(document).ready(function () {
-//   $('#uno').mouseleave(function () {
-//     $('#dark-screen1').hide(0);
-//   });
-// });
-
-
 
 $(document).ready(function () {
   $('#deuce').mouseover(function () {
     $('#dark-screen2').show(1200);
-    $('#deuce').mouseleave(function(){
+    $('#deuce').mouseleave(function () {
       $('#dark-screen2').hide(1200);
     });
   });
 });
 
 
-// $(document).ready(function () {
-//   $('#dark-screen2').mouseleave(function () {
-//     $('#deuce').show(0, function () {
-//       $('#dark-screen2').hide(0);
-//     });
-//   });
-// });
-
 
 $(document).ready(function () {
   $('#tres').mouseover(function () {
     $('#dark-screen3').show(1200);
-    $('#tres').mouseleave(function(){
+    $('#tres').mouseleave(function () {
       $('#dark-screen3').hide(1200);
     });
   });
 });
 
-// $(document).ready(function () {
-//   $('#dark-screen3').mouseleave(function () {
-//     $('#tres').show(0, function () {
-//       $('#dark-screen3').hide(0);
-//     });
-//   });
-// });
 
 
 $(document).ready(function () {
   $('#cuatro').mouseover(function () {
-    $('#dark-screen4').show(1200,function(){
-      $('#cuatro').mouseleave(function(){
+    $('#dark-screen4').show(1200, function () {
+      $('#cuatro').mouseleave(function () {
         $('#dark-screen4').hide(1200);
       });
     });
   });
 });
 
-// $(document).ready(function () {
-//   $('#dark-screen4').mouseleave(function () {
-//     $('#cuatro').show(0, function () {
-//       $('#dark-screen4').hide(0);
-//     });
-//   });
-// });
-
 
 
 $(document).ready(function () {
   $('#uno2').mouseover(function () {
     $('#dark-screen5').show(1200);
-    $('#uno2').mouseleave(function(){
+    $('#uno2').mouseleave(function () {
       $('#dark-screen5').hide(1200);
     });
   });
 });
 
-// $(document).ready(function () {
-//   $('#dark-screen5').mouseleave(function () {
-//     $('#uno2').show(0, function () {
-//       $('#dark-screen5').hide(0);
-//     });
-//   });
-// });
-
-
 
 $(document).ready(function () {
   $('#deuce2').mouseover(function () {
     $('#dark-screen6').show(1200);
-    $('#deuce2').mouseleave(function(){
+    $('#deuce2').mouseleave(function () {
       $('#dark-screen6').hide(1200);
     });
   });
 });
 
-// $(document).ready(function () {
-//   $('#dark-screen6').mouseleave(function () {
-//     $('#deuce2').show(0, function () {
-//       $('#dark-screen6').hide(0);
-//     });
-//   });
-// });
-
 
 $(document).ready(function () {
   $('#tres2').mouseover(function () {
     $('#dark-screen7').show(1200);
-    $('#tres2').mouseleave(function(){
+    $('#tres2').mouseleave(function () {
       $('#dark-screen7').hide(1200);
     });
   });
 });
 
-// $(document).ready(function () {
-//   $('#dark-screen7').mouseleave(function () {
-//     $('#tres2').show(0, function () {
-//       $('#dark-screen7').hide(0);
-//     });
-//   });
-// });
-
 
 $(document).ready(function () {
   $('#cuatro2').mouseover(function () {
     $('#dark-screen8').show(1200);
-    $('#cuatro2').mouseleave(function(){
+    $('#cuatro2').mouseleave(function () {
       $('#dark-screen8').hide(1200);
     });
   });
 });
-
-// $(document).ready(function () {
-//   $('#dark-screen8').mouseleave(function () {
-//     $('#cuatro2').show(0, function () {
-//       $('#dark-screen8').hide(0);
-//     });
-//   });
-// });
-
-
-
-// $(document).ready(function(){
-//   $(".uno img").click(function(){
-//     $("#dark-screeen1").show(000);
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".deuce").click(function(){
-//     $("#dark-screeen2").show(000);
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".tres").click(function(){
-//     $("#dark-screeen3").show(000);
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".cuatro").click(function(){
-//     $("#dark-screeen4").show(000);
-//   });
-// });
-
-
-// $(document).ready(function(){
-//   $(".uno2").click(function(){
-//     $("#dark-screeen5").show(000);
-//   });
-// });
-
-
-// $(document).ready(function(){
-//   $(".deuce2").click(function(){
-//     $("#dark-screeen6").show(000);
-//   });
-// });
-
-
-// $(document).ready(function(){
-//   $(".tres2").click(function(){
-//     $("#dark-screeen7").show(000);
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".cuatro2").click(function(){
-//     $("#dark-screeen8").show(000);
-//   });
-// });
-
-
-
-
-// $(document).ready(function(){
-//     $('#designs-first').click(function(){
-//         $('#design-image').show(1000);
-//     });
-// });
-
-// $(document).ready(function(){
-//     $('#designs-portfolio').click(function(){
-//         $('#portfolio-image').show(1000);
-//     });
-// });
-
-// $(document).ready(function(){
-//     $('#designs-product').click(function(){
-//         $('#product-image').show(1000);
-//     });
-// });
-
-
-
-
-
-
-// $(document).ready(function(){
-//     $('#designs-first').click(function(){
-//         $('#designs-first').toggle();
-//     });
-// });
-
-// $(document).ready(function(){
-//     $('#designs-first').click(function(){
-//         $('#designs-first').hide(0,function(){
-//             $('#design-image').show();
-//         });
-//     });
-// });
-
-// $(document).ready(function(){
-//     $('#designs-portfolio').click(function(){
-//         $('#designs-portfolio').hide(0,function(){
-//             $('#portfolio-image').toggle(0,function(){
-//                 $('#design-image').click(0,function(){
-//                     $('#designs-first').toggle(0,function(){
-//                         $('#design-image').toggleOut();
-//                     });
-//                 });
-//             });
-//         });
-//     });
-// });
-
-
-// $(document).ready(function(){
-//     $('#designs-product').click(function(){
-//         $('#designs-product').hide(0,function(){
-//             $('#productdev-image').toggle(0,function(){
-//                 $('#design-image').click(0,function(){
-//                     $('#designs-first').hide(0,function(){
-//                         $('#design-image').show();
-//                     });
-//                 });
-//             });
-//         });
-//     });
-// });
